6e1498f9c0aae6ba092526617ed00a269e37125a
Halit Celik <halit@cruncher.ch>
2024-03-22 18:18:33 +0100

    make it a module and custom element

--- passkeys/static/passkeys/js/passkey-login.js
+++ passkeys/static/passkeys/js/passkey-login.js
@@ -1,10 +1,161 @@
-import element from "https://stephen.band/dom/modules/element.js";
+import element, { getInternals } from "https://stephen.band/dom/modules/element.js";
+import { getServerCredentials, startAuthn, getAssertReq, credToJSON, assertFunc } from "./script.js"
+function checkSecureConnection() {
+    if (window.location.protocol != 'https:') {
+        console.error("Passkeys must work under secure context");
+    }
+    return window.location.protocol == 'https:';
+}
 
-export element(
+function initialize(submitButtons, form, passkeyInput) {
+    submitButtons.forEach(button => {
+        button.disabled = true;
+    });
+    getServerCredentials()
+        .then(data => {
+            startAuthn(getAssertReq(credToJSON(data))).then(assertion => {
+                assertFunc(assertion, form, passkeyInput)
+            })
+        })
+        .catch(error => {
+            submitButtons.forEach(button => {
+                button.disabled = false;
+            })
+            console.error('Error during login:', error);
+        });
+}
+
+export default element(
     "passkey-login", {
+    shadow: `
+    <form class="login-form" id="login-form" method="post" action="." data-bitwarden-watching="1">
+        <input type="hidden" name="csrfmiddlewaretoken" value="">
+        <div class="form-row">
+        </div>
+        <div class="form-row">
+            <label for="id_email">Your email adress:</label> <protonpass-root-da79 data-protonpass-role="icon"></protonpass-root-da79><input type="email" name="email" value="halit@cruncher.ch" autofocus="" autocomplete="username webauthn" maxlength="320" required="" id="id_email" style="padding-right: 35.3333px !important;" data-protonpass-base-css="{&quot;padding-right&quot;:&quot;&quot;}">
+        </div>
+        <div class="form-row"> 
+            <label for="id_password">Password:</label> <input type="password" name="password" id="id_password">
+            <input type="hidden" name="next" value="/passkeys/">
+        </div>
+        <div class="submit-row">
+            <input type="submit" value="Log in">
+            <input id="passkeys" type="hidden" name="passkeys">
+            <input type="submit" data-passkey-value="passkey" value="Login with passkey">
+            <input type="submit" data-passkey-value="otp" value="Receive email code">
+            <input type="hidden" value="" name="type">
+        </div>
+    </form>
+    `,
     construct: function (shadow, internals) {
-        console.log("CONSTRUCT", this, shadow, internals)
-    },
+        /* Find all the elements */
+        const form = shadow.querySelector("#login-form");
+        const submitButtons = shadow.querySelectorAll("input[type='submit']");
+        const passkeyInput = shadow.querySelector("#passkeys");
 
-}, {});
+        /* Prevend default submission and modify action attribute */
+        form.addEventListener("click", e => {
+            let element = e.target;
+            if (element.dataset.passkeyValue && element.dataset.passkeyValue) {
+                e.preventDefault();
+                let form = element.closest("form");
+                form.querySelector("#id_password").required = false;
+                form.querySelector("input[name='type']").value = element.dataset.passkeyValue;
+                form.action = passkeysConfig.urls.login[element.dataset.passkeyValue];
+                form.submit();
+            }
+        })
+        /* Initialize passkey */
+        if (checkSecureConnection()) {
+            initialize(submitButtons, form, passkeyInput);
+        }
+    },
+    connect: function (shadow, internals) {
+        console.log("CONNECT", this, shadow, internals)
+        const csrfInput = shadow.querySelector('[name="csrfmiddlewaretoken"]');
+        csrfInput.value = internals.csrftoken;
+        internals.styleurls.split(" ").forEach(url => {
+            const linkElem = document.createElement("link");
+            linkElem.setAttribute("rel", "stylesheet");
+            linkElem.setAttribute("href", url);
+            shadow.appendChild(linkElem);
+        })
+
+    }
+
+}, {
+    crossplatform: {
+        attribute: function (value) {
+            const internals = getInternals(this);
+            internals.crossplatform = parseInt(value);
+        }
+    },
+    loginurl: {
+        attribute: function (url) {
+            const internals = getInternals(this);
+            internals.loginurl = url;
+        }
+    },
+    homeurl: {
+        attribute: function (url) {
+            const internals = getInternals(this);
+            internals.homeurl = url;
+        }
+    },
+    authbeginurl: {
+        attribute: function (url) {
+            const internals = getInternals(this);
+            internals.authbeginurl = url;
+        }
+    },
+    regbeginurl: {
+        attribute: function (url) {
+            const internals = getInternals(this);
+            internals.regbeginurl = url;
+        }
+    },
+    regcompleteurl: {
+        attribute: function (url) {
+            const internals = getInternals(this);
+            internals.regcompleteurl = url;
+        }
+    },
+    delkeyurl: {
+        attribute: function (url) {
+            const internals = getInternals(this);
+            internals.delkeyurl = url;
+        }
+    },
+    toggleurl: {
+        attribute: function (url) {
+            const internals = getInternals(this);
+            internals.toggleurl = url;
+        }
+    },
+    passkeyloginurl: {
+        attribute: function (url) {
+            const internals = getInternals(this);
+            internals.passkeyloginurl = url;
+        }
+    },
+    otploginurl: {
+        attribute: function (url) {
+            const internals = getInternals(this);
+            internals.otploginurl = url;
+        }
+    },
+    csrf: {
+        attribute: function (csrftoken) {
+            const internals = getInternals(this);
+            internals.csrftoken = csrftoken;
+        }
+    },
+    styleurls: {
+        attribute: function (styles) {
+            const internals = getInternals(this);
+            internals.styleurls = styles;
+        }
+    }
+});
 
--- passkeys/static/passkeys/js/script.js
+++ passkeys/static/passkeys/js/script.js
@@ -1,3 +1,4 @@
+
 const base64urlEncode = (array) => {
     let arrayBuf = array
     if (typeof array === "object") {
@@ -22,33 +23,23 @@ function arrayToBuffer(array) {
     return array
 }
 
-function assertFunc(assertion) {
-    pk_input = document.getElementById("passkeys");
-    if (pk_input.length == 0) {
-        console.error("Did you add the 'passkey' hidden input field")
-        return
-    }
+export function assertFunc(assertion, form, pkInput) {
     if (assertion.rawId) {
         assertion.rawId = arrayToBuffer(assertion.rawId);
     }
     for (let key in assertion.response) {
-        /*Careful about null values. On PC worked fine but on the android got null value */
+        /* Careful about null values. On PC worked fine but on the android got null value */
         if (!Object.is(assertion.response[key], null)) {
             assertion.response[key] = arrayToBuffer(assertion.response[key])
         }
     }
 
-    pk_input.value = JSON.stringify(credToJSON(assertion));
-    let form = document.getElementById("login-form")
+    pkInput.value = JSON.stringify(credToJSON(assertion));
     form.action = window.passkeysConfig.urls.login.passkey;
-    if (form === null || form === undefined) {
-        console.error("Did you pass the correct form id to auth function")
-        return;
-    }
     form.submit()
 }
 
-const credToJSON = (pubKeyCred) => {
+export const credToJSON = (pubKeyCred) => {
     if (pubKeyCred instanceof Array) {
         return pubKeyCred.map(credToJSON);
     }
@@ -65,7 +56,7 @@ const credToJSON = (pubKeyCred) => {
     return pubKeyCred;
 };
 
-const getAssertReq = (getAssert) => {
+export const getAssertReq = (getAssert) => {
     console.log(getAssert)
     getAssert.publicKey.challenge = base64urlDecode(getAssert.publicKey.challenge);
     for (const allowCred of getAssert.publicKey.allowCredentials) {
@@ -74,8 +65,7 @@ const getAssertReq = (getAssert) => {
     return getAssert;
 };
 
-// Define function to handle registration request
-const makeCredReq = (creds) => {
+export const makeCredReq = (creds) => {
     creds.publicKey.challenge = base64urlDecode(creds.publicKey.challenge);
     creds.publicKey.user.id = base64urlDecode(creds.publicKey.user.id);
     for (const excludeCred of creds.publicKey.excludeCredentials) {
@@ -83,7 +73,7 @@ const makeCredReq = (creds) => {
     }
     return creds;
 };
-function getServerCredentials() {
+export function getServerCredentials() {
     return new Promise((resolve, reject) => {
         fetch(window.passkeysConfig.urls.authBegin)
             .then(response => {
@@ -100,13 +90,14 @@ function getServerCredentials() {
             });
     });
 }
-function startAuthn(options, conditionalUI) {
+export function startAuthn(options, conditionalUI) {
     if (conditionalUI) {
         options.mediation = 'conditional';
         options.signal = window.conditionUIAbortSignal;
     }
     else
         window.conditionUIAbortController.abort()
+    console.log(options)
     return navigator.credentials.get(options);
 }
 const beginReg = () => {
@@ -180,38 +171,11 @@ function toggleKey(id) {
 }
 
 
-function startRegistration() {
-    const modalTitle = document.querySelector("#modal-title");
-    const modalBody = document.querySelector("#modal-body");
-
-    modalTitle.innerHTML = "Enter a token name";
-    modalBody.innerHTML = `<p>Please enter a name for your new token</p>
-                            <input type="text" placeholder="e.g Laptop, PC" id="key_name" class="form-control"/><br/>
-                            <div id="res"></div>`;
-    const modalFooter = document.querySelector("#modal-footer");
-    modalFooter.insertAdjacentHTML('afterbegin', `<button id='actionBtn' class='btn btn-success' onclick="djangoPasskey.beginReg()">Start</button>`);
-    document.querySelector("#popUpModal").style.display = "block";
-}
-
-function initialize() {
-    getServerCredentials()
-        .then(data => {
-            startAuthn(getAssertReq(credToJSON(data))).then((assertion) => {
-                assertFunc(assertion);
-            });
-        })
-        .catch(error => {
-            console.error('Error during login:', error);
-        });
-}
-
-
 
 window.djangoPasskey = {
-    startRegistration: startRegistration,
     deleteKey: deleteKey,
+    toggleKey: toggleKey,
     beginReg: beginReg,
-    initialize: initialize
 };
 
 
@@ -219,13 +183,11 @@ window.conditionalUI = false;
 window.conditionUIAbortController = new AbortController();
 window.conditionUIAbortSignal = window.conditionUIAbortController.signal;
 
-
 // Function to initiate authentication
 function authn(formId) {
     startAuthn(formId, false);
 }
 
-/* TODO */
 function displayPasskeyOption() {
     // Availability of `window.PublicKeyCredential` means WebAuthn is usable.
     // `isUserVerifyingPlatformAuthenticatorAvailable` means the feature detection is usable.
