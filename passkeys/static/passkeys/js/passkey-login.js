180748fc4363117411b03db83fcc908be80f5f1f
Halit Celik <halit@cruncher.ch>
2024-03-22 16:00:34 +0100

    passkey as custom element initial

---
+++ passkeys/static/passkeys/js/passkey-login.js
@@ -0,0 +1,10 @@
+import element from "https://stephen.band/dom/modules/element.js";
+
+export element(
+    "passkey-login", {
+    construct: function (shadow, internals) {
+        console.log("CONSTRUCT", this, shadow, internals)
+    },
+
+}, {});
+
